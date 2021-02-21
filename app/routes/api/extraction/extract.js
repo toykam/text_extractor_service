835c9545a6aaa7ee8a9120876a2c1bf2cc126887
toykam <mwork063@gmail.com>
2021-02-21 13:58:45 +0100

    Can now extract text from multiple image at a time

--- app/routes/api/extraction/extract.js
+++ app/routes/api/extraction/extract.js
@@ -17,33 +17,41 @@ module.exports = (express, db) => {
                         if (!req.files) {
                             res.send({error: 'No file selected'})
                         } else {
-                            const imageData = req.files.image
-                            // const uploadPath = __dirname+'../../../../../public/images/'+imageData.name
-                            const uploadPath = path.join(__dirname, '/../../../../public/images/', imageData.name)
-            
-                            imageData.mv(uploadPath, (err) => {
-                                if (err) {
-                                    res.send({error: `An error occurred, Please try again later ${err}`})
-                                } else {
-                                    const worker = createWorker({
-                                        langPath: path.join(__dirname, './', 'lang-data'), 
-                                        logger: m => console.log(m),
-                                        errorHandler: error => console.log(error),
-                                    });
-                                    
-                                    (async () => {
-                                        await worker.load();
-                                        await worker.loadLanguage(req.params.lang);
-                                        await worker.initialize(req.params.lang);
-                                        const { data: { text } } = await worker.recognize(uploadPath);
-                                        await worker.terminate();
-                                        console.log(text);
-                                        res.send({
-                                            name: imageData.name, path: uploadPath, text
-                                        })
-                                    })();
-                                    
-                                }
+                            var extractedTexts = []
+
+                            const images = req.files.images
+                            console.log(images)
+                            // const uploadPath = __dirname+'../../../../../public/images/'+images.name
+
+                            images.forEach((image, index) => {
+                                const uploadPath = path.join(__dirname, '/../../../../public/images/', image.name)
+                
+                                image.mv(uploadPath, (err) => {
+                                    if (err) {
+                                        res.send({error: `An error occurred, Please try again later ${err}`})
+                                    } else {
+                                        const worker = createWorker({
+                                            langPath: path.join(__dirname, './', 'lang-data'), 
+                                            logger: m => console.log(m),
+                                            errorHandler: error => console.log(error),
+                                        });
+
+                                        (async () => {
+                                            await worker.load();
+                                            await worker.loadLanguage(req.params.lang);
+                                            await worker.initialize(req.params.lang);
+                                            const { data: { text } } = await worker.recognize(uploadPath);
+                                            await worker.terminate();
+                                            console.log(text);
+                                            extractedTexts.push({
+                                                name: image.name, path: uploadPath, text
+                                            });
+                                            if (images.length == extractedTexts.length) {
+                                                res.send(extractedTexts)
+                                            }
+                                        })();
+                                    }
+                                })
                             })
             
                         }
