7d014a939a64e96fcfb48c0bd1ff3de3cf65997b
toykam <mwork063@gmail.com>
2021-02-21 10:33:06 +0100

    Fixed port issue

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fileUpload = require('express-fileupload')
 
 const app = express();
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
