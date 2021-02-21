83f46eac5b01e5c8fe45d1e2a82c7e497e909c5a
toykam <mwork063@gmail.com>
2021-02-21 10:20:42 +0100

    added language support

--- app/routes/api/extraction/extract.js
+++ app/routes/api/extraction/extract.js
@@ -6,38 +6,49 @@ module.exports = (express, db) => {
     const router = express.Router()
     router.post('/:lang', async (req, res) => {
         try {
-            if (!req.files) {
-                res.send({error: 'No file selected'})
-            } else {
-                const imageData = req.files.image
-                const uploadPath = __dirname+'../../../../../public/images/'+imageData.name
-
-                imageData.mv(uploadPath, (err) => {
-                    if (err) {
-                        res.send({error: `An error occurred, Please try again later ${err}`})
+            db.collection('langs').findOne({key: req.params.lang}, (error, result) => {
+                console.log(result);
+                if (error) {
+                    res.send({error: 'The select language is not supported'})
+                } else {
+                    if (!result) {
+                        res.send({error: 'The select language is not supported'})
                     } else {
-                        const worker = createWorker({
-                            langPath: path.join(__dirname, './', 'lang-data'), 
-                            logger: m => console.log(m),
-                            errorHandler: error => console.log(error),
-                        });
-                        
-                        (async () => {
-                            await worker.load();
-                            await worker.loadLanguage(req.params.lang);
-                            await worker.initialize(req.params.lang);
-                            const { data: { text } } = await worker.recognize(uploadPath);
-                            await worker.terminate();
-                            console.log(text);
-                            res.send({
-                                name: imageData.name, path: uploadPath, text
+                        if (!req.files) {
+                            res.send({error: 'No file selected'})
+                        } else {
+                            const imageData = req.files.image
+                            const uploadPath = __dirname+'../../../../../public/images/'+imageData.name
+            
+                            imageData.mv(uploadPath, (err) => {
+                                if (err) {
+                                    res.send({error: `An error occurred, Please try again later ${err}`})
+                                } else {
+                                    const worker = createWorker({
+                                        langPath: path.join(__dirname, './', 'lang-data'), 
+                                        logger: m => console.log(m),
+                                        errorHandler: error => console.log(error),
+                                    });
+                                    
+                                    (async () => {
+                                        await worker.load();
+                                        await worker.loadLanguage(req.params.lang);
+                                        await worker.initialize(req.params.lang);
+                                        const { data: { text } } = await worker.recognize(uploadPath);
+                                        await worker.terminate();
+                                        console.log(text);
+                                        res.send({
+                                            name: imageData.name, path: uploadPath, text
+                                        })
+                                    })();
+                                    
+                                }
                             })
-                        })();
-                        
+            
+                        }
                     }
-                })
-
-            }
+                }
+            })
         } catch {
             res.send('An error occurred, while extracting text, please try again after some time')
         }
